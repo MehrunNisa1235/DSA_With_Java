3e7d4fd375edbc67acf622a2bb11d9500d609775
MEHRUN NISA <119696663+MehrunNisa1235@users.noreply.github.com>
2023-05-20 10:31:38 +0530

    Create StringImmutability.java

---
+++ StringImmutability.java
@@ -0,0 +1,14 @@
+public class StringImmutability {
+      
+    public static void main(String[] args) {
+          
+        String district_Ram = "Mumbai";
+        String district_Shyam = "Mumbai";
+        String district_Laxman = "Mumbai";
+        String district_Sita = "Mumbai"; 
+ 
+       district_Ram = "Kolkata";  
+       System.out.println(district_Ram);  
+ 
+    }
+}
