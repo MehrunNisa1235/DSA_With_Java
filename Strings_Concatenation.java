3bc3b320a210a7b5ac93778c40fd8d85f341bfa3
MEHRUN NISA <119696663+MehrunNisa1235@users.noreply.github.com>
2023-05-20 10:31:01 +0530

    Create Strings_Concatenation.java

---
+++ Strings_Concatenation.java
@@ -0,0 +1,15 @@
+import java.util.*; 
+
+// concatenate(join the strings)
+public class Strings_Concatenation
+{ 
+    public static void main(String args[])
+    {
+        String s1="mehrun";
+        String s2="nisa";
+       String concatenate=s1 + " " + s2;
+
+    System.out.println("String is : " +concatenate); 
+    }
+    
+}
