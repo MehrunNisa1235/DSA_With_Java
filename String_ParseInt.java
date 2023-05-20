13f1cc7a7a8eff0697ab87a1628497465169784a
MEHRUN NISA <119696663+MehrunNisa1235@users.noreply.github.com>
2023-05-20 10:32:44 +0530

    Create String_ParseInt.java

---
+++ String_ParseInt.java
@@ -0,0 +1,11 @@
+public class String_ParseInt {
+   public static void main(String args[]) {
+       String str = "123";
+       int number = Integer.parseInt(str);
+       System.out.println(number);
+
+      // System.out.println(str);
+      
+      
+   }
+}
