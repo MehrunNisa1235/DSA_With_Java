9c22394a73a7c425f019545c529defa83aa2b3ae
MEHRUN NISA <119696663+MehrunNisa1235@users.noreply.github.com>
2023-05-20 10:30:25 +0530

    Create Strings_Length.java

---
+++ Strings_Length.java
@@ -0,0 +1,16 @@
+import java.util.*; 
+
+// fing the String length:- 
+
+public class Strings_Length
+{ 
+    public static void main(String args[])
+    {
+        String s1="mehrun";
+        String s2="nisa";
+       String concatenate=s1 + " " + s2;
+
+    System.out.println("length of the String is  : " +concatenate.length()); 
+    }
+    
+}
