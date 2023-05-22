88ea8b070210d8d4dcd94d2148a463f2a0fbcaa0
MEHRUN NISA <119696663+MehrunNisa1235@users.noreply.github.com>
2023-05-22 18:36:44 +0530

    Create Reverse_DoublyLL.java

---
+++ Reverse_DoublyLL.java
@@ -0,0 +1,97 @@
+//Reverse the Doubly Linked List:- 
+
+public class Reverse_DoublyLL
+{
+   public static class Node
+   {
+       int data;
+       Node next; 
+       Node prev; 
+
+       public Node(int data)
+       {
+          this.data=data;
+          this.next=null; 
+          this.prev=null; 
+          
+       }
+   }
+
+       public static Node head;
+       public static Node tail; 
+       
+
+ 
+       //AddFirst 
+       public void addFirst(int data)
+       {
+            //step1: create a new node 
+            Node newNode=new Node(data);
+       
+            if(head == null)
+            {
+                head=tail=newNode;
+                    return; 
+            }
+
+            //step2: newNode next = head;
+            newNode.next=head; 
+
+            //step3: head=newNode
+            head=newNode; 
+       }
+
+       //reverse:- 
+       public static void reverse()
+       {
+            
+            Node curr=head;
+            Node prev=null; 
+            Node next;
+
+            while(curr!=null)
+            {
+                next=curr.next;
+                curr.next=prev;
+                curr.prev=next; 
+
+                prev=curr;
+                curr=next; 
+            }
+              head=prev; 
+       }
+
+
+       public void print()
+       {
+            if(head==null)
+            {
+                System.out.print("LinkedList is empty "); 
+                  return;
+            }
+
+            Node temp=head;   //create a temporary node
+            while(temp!=null)
+            {
+                System.out.print(temp.data +"->");
+                temp=temp.next;
+            }
+            System.out.println("null"); 
+       }
+       
+
+
+    public static void main(String args[])
+    {
+        Reverse_DoublyLL ll = new Reverse_DoublyLL(); 
+        ll.addFirst(2); 
+        ll.addFirst(3);
+        ll.addFirst(5);
+        ll.addFirst(8);
+        ll.addFirst(10);
+        ll.print();
+        
+        ll.reverse();
+        ll.print(); 
+    }
+}
