7d863ede756829de572d5cb4c47ecf43ed620f54
MEHRUN NISA <119696663+MehrunNisa1235@users.noreply.github.com>
2023-05-28 19:19:26 +0530

    Create StackBottom.java

---
+++ StackBottom.java
@@ -0,0 +1,32 @@
+// Insert Element in Bottom of the stack:- 
+
+import java.util.*; 
+public class StackBottom
+{
+    public static void  pushAtBottom(Stack<Integer> s, int data)
+    {
+        if(s.isEmpty())
+        {
+            s.push(data); 
+            return; 
+
+        }
+        int top=s.pop();
+        pushAtBottom(s,data); 
+        s.push(top); 
+    }
+
+    public static void main(String args[])
+    {
+        Stack<Integer> s = new Stack<>(); 
+        s.push(10);
+        s.push(20);
+        s.push(30); 
+        s.push(40); 
+        pushAtBottom(s,7); 
+        while(!s.isEmpty())
+        {
+            System.out.println(s.pop()); 
+        }
+    }
+}
