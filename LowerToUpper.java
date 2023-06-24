e3cfda95e6e776d932584b857728bc5991e9abc8
MEHRUN NISA <119696663+MehrunNisa1235@users.noreply.github.com>
2023-06-24 15:54:35 +0530

    Create LowerToUpper.java

---
+++ LowerToUpper.java
@@ -0,0 +1,16 @@
+Example 1:
+
+Input:
+str = "geeks"
+Output: GEEKS
+
+
+//User function Template for Java
+class Solution 
+{ 
+    String to_upper(String str) 
+    { 
+        // code here
+        return str.toUpperCase();
+    }
+} 
