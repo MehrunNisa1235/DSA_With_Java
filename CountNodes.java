2ef1455f7fd967acd28693db9f8d7faf849b4f15
MEHRUN NISA <119696663+MehrunNisa1235@users.noreply.github.com>
2023-09-30 23:18:28 +0530

    Create CountNodes.java

---
+++ CountNodes.java
@@ -0,0 +1,58 @@
+// count number of nodes in Binary Tree :- 
+
+import java.util.*;
+public class CountNodes
+{
+    static class Node
+    {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree
+    {
+        static int idx =-1;
+        public static Node buildTree(int nodes[])
+        {
+            idx++;
+            if(nodes[idx] == -1)
+            {
+                return null;
+            }
+            
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+            
+    }
+     
+    public static int countNumberOfNodes(Node root)
+    {
+       if(root == null)
+       {
+            return 0;
+       }
+
+       int leftNodes = countNumberOfNodes(root.left);
+       int rightNodes = countNumberOfNodes(root.right);
+       return leftNodes + rightNodes + 1;
+    }
+    public static void main(String args[])
+    {
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(countNumberOfNodes(root));
+    }
+}  
